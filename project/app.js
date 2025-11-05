567dbbd30e9775a8aee4d8c11e569840806d80ae
Felix <linlett.lltt@gmail.com>
2025-11-05 20:38:32 +0900

    Use environment variable for server port

--- project/app.js
+++ project/app.js
@@ -14,7 +14,7 @@ const itemRoutes = require('./routes/items');
 app.use('/', itemRoutes);
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
